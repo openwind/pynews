63f96d48e09a8c2cfcaa8eb490e0931eba66a666
zhida Pan <zhidapan@foxmail.com>
2015-12-25 23:30:36 -0800

    add relevant search code.

---
+++ relevant_search/key_word.py
@@ -0,0 +1,9 @@
+#-*-coding:utf-8-*-
+import json
+import codecs
+
+key_word = u"安啦"
+
+fout = codecs.open("key_word", "w", "utf-8")
+fout.write(json.dumps(key_word, ensure_ascii=False, encoding='utf-8'))
+fout.close()
