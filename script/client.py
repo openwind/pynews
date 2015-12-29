4acd8b566210a8630c5f8316b668a4edd26504c0
genialwang <colorfishes@126.com>
2015-12-29 15:31:48 +0800

    精简代码，添加接收前端传回信息功能
    现在可以接收前端传回的信息用于具体的搜索和排序，搜索结果返回之后使用借口取得数据渲染
    结果页面，返回结果

--- script/client.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__author__ = 'Genial Wang'
-
-
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return '<h1>Hello, web!</h1>'
